5d5ba097392b26dd1b2cb90d291eeaec854f9723
lewardo <segfault.ninja@gmail.com>
2023-07-31 14:32:23 +0100

    added background images

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
+import { RouterView } from 'vue-router'
+
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
-  <Header></Header>
-  <RouterView />
-  <Footer></Footer>
+  <div class="app-background">
+    <img src="@/assets/img/bg_ne.png" id="bg_ne" draggable="false">
+    <img src="@/assets/img/bg_nw.png" id="bg_nw" draggable="false">
+    <img src="@/assets/img/bg_se.png" id="bg_se" draggable="false">
+    <img src="@/assets/img/bg_sw.png" id="bg_sw" draggable="false">
+  </div>
+  <div class="app-content">
+    <Header></Header>
+    <RouterView />
+    <Footer></Footer>
+  </div>
 </template>
 
 <style scoped>
