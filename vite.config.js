6c6bb30564ee1472af572377e18e36ca2d96cf39
lewardo <segfault.ninja@gmail.com>
2023-07-31 16:16:27 +0100

    mb you can only host from ./docs

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: '../docs', // so github pages doesnt have a stronk
     rollupOptions: {
       output: {
         entryFileNames: `assets/js/[name].js`,
