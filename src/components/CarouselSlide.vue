c66867a11513ca37947ecad7d818c04696db58ac
lewardo <segfault.ninja@gmail.com>
2023-07-31 16:05:36 +0100

    added carousel components

---
+++ src/components/CarouselSlide.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    prev: {
+        type: String,
+        required: true
+    },
+    next: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <li :id="name" class="carousel-slide">
+        <a :href="prev" class="carousel-prev"></a>
+        <div class="carousel-content">
+            <slot />
+        </div>
+        <a :href="next" class="carousel-next"></a>
+    </li>
+</template>
+
+<style scoped>
+    .carousel-slide {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        position: relative;
+        flex: 0 0 100%;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        scroll-snap-align: center;
+    }
+
+    .carousel-prev,
+    .carousel-next {
+        z-index: 1;
+        width: 10%;
+        height: 100%;
+    }
+
+    .carousel-content {
+        width: 80%;
+        height: 100%;
+        border-radius: 24px;
+        overflow: hidden;
+    }
+</style>
---
+++ src/components/CarouselWrapper.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="carousel">
+        <ul class="carousel-viewport">
+            <slot />
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .carousel {
+        width: 100%;
+        height: 100%;
+        position: relative;
+    }
+
+    .carousel::before {
+        content: '\276E';
+        font-weight: bolder;
+        color: #fff;
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 10%;
+        transform: translateY(-50%);
+        text-align: center;
+        font-size: 32px;
+    }
+
+    .carousel::after {
+        content: '\276F';
+        font-weight: bolder;
+        color: #fff;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        width: 10%;
+        transform: translateY(-50%);
+        text-align: center;
+        font-size: 32px;
+    }
+
+    .carousel-viewport {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        overflow-x: scroll;
+        overflow-y: hidden;
+        scroll-behavior: smooth;
+        scroll-snap-type: x mandatory;
+        scrollbar-width: 0;
+        -ms-overflow-style: 0;
+    }
+
+    .carousel-viewport::-webkit-scrollbar {
+        display: none;
+    }
+</style>
