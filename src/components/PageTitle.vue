dd3207b7a6dda60ecfb56d8a50aab85883493184
lewardo <segfault.ninja@gmail.com>
2023-08-06 14:56:44 +0100

    fix firefox issue

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,26 +1,30 @@
 <script setup>
 defineProps({
-  title: {
-    type: String,
-    required: true
-  },
-  subtitle: {
-    type: Boolean,
-    required: false,
-    default: true
-  },
-  schedule: {
-    type: Boolean,
-    required: false,
-    default: false
-  }
+    title: {
+        type: String,
+        required: true
+    },
+    subtitle: {
+        type: Boolean,
+        required: false,
+        default: true
+    },
+    schedule: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
 })
 </script>
 
 <template>
     <div class="title">
         <a href="/">
-            <img src="@/assets/img/title.png" class="title-img" alt="sexpress yourself conference banner" draggable="false">
+            <img
+                src="@/assets/img/title.png"
+                class="title-img"
+                alt="sexpress yourself conference banner"
+                draggable="false" />
         </a>
         <div v-if="subtitle" class="subtitle">
             <span>{{ title }}</span>
@@ -32,49 +36,52 @@ defineProps({
 </template>
 
 <style scoped>
-    .title {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+.title {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
-    .title > a {
-        max-height: 30vh;
-    }
+.title > a {
+    max-height: 30vh;
+    display: flex;
+    place-items: center;
+    justify-content: center;
+}
 
+.title-img {
+    height: 100%;
+}
+
+@media screen and (max-aspect-ratio: 5/4) {
     .title-img {
-        height: 100%;
+        max-width: 80vw;
     }
+}
 
-    @media screen and ( max-aspect-ratio: 5/4 ) {
-        .title-img {
-            max-width: 80vw;
-        }
-    }
+.subtitle {
+    width: 100%;
+    line-height: min(6vh, 8vw);
+    font-size: min(3vh, 4vw);
+    display: flex;
+    justify-content: center;
+    margin: 1vh 0;
+}
 
-    .subtitle {
-        width: 100%;
-        line-height: min(6vh, 8vw);
-        font-size: min(3vh, 4vw);
-        display: flex;
-        justify-content: center;
-        margin: 1vh 0;
-    }
+.subtitle > span {
+    color: #fff;
+    background-color: #0f0a62;
+    padding: 0 min(24px, 5vw);
+    border-radius: 100vw;
+}
 
-    .subtitle > span {
-        color: #fff;
-        background-color: #0f0a62;
-        padding: 0 min(24px, 5vw);
-        border-radius: 100vw;
-    }
-
-    .subtitle a {
-        color: #fff;
-        background-color: #ff3131;
-        text-decoration: none;
-        padding: 0 min(24px, 5vw);
-        margin: 0 0 0 12px;
-        border-radius: 100vw;
-    }
+.subtitle a {
+    color: #fff;
+    background-color: #ff3131;
+    text-decoration: none;
+    padding: 0 min(24px, 5vw);
+    margin: 0 0 0 12px;
+    border-radius: 100vw;
+}
 </style>
