47632425b9e0f2ac5bb57b6b2f1ce914c784561e
lewardo <segfault.ninja@gmail.com>
2023-08-26 16:08:42 +0100

    initial about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,7 +6,10 @@ import PageTitle from "@/components/PageTitle.vue"
     <PageTitle title="About" />
 
     <div class="content">
-        Sexpression:&#8203;Newcastle is a thing
+        <div class="info">
+            Sexpression:&#8203;Newcastle form part of the UK-wide sex-education charity <a href="https://sexpression.org.uk/">Sexpression:UK</a>. We are a UK-based, near-peer charity who run comprehensive RSE in the community, with the goal of empowering young people to make decisions about relationships and sex.
+            This year, Newcastle are the nominated hosts of the Sexpression:UK National Conference 2023, on the 4th and 5th of November, with the theme '(S)express Yourself'. This will provide opportunities for sex-positive education and expression for university students and adults. We hope to welcome up to 120 attendees, made up of Sexpression members and alumni, as well as members of the general public interested in sexual health.
+        </div>
     </div>
 </template>
 
@@ -15,13 +18,15 @@ import PageTitle from "@/components/PageTitle.vue"
 
 .content {
     width: 90%;
-    display: flex;
-    font-size: min(6vw, 4vh);
+    font-size: 2.5vmin;
     color: #fff;
     border-radius: 24px;
     overflow: hidden;
     background: #1a60d688;
-    padding: 2vh;
+    padding: 1.5vh;
+    text-align: center;
+    display: grid;
+    place-items: center;
 }
 
 </style>
